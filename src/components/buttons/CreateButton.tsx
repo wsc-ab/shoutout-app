ca79fb040c1efaf0cfada3add3155a0c98427c67
Won Sang Choi <wonsang.choi@airballoon.app>
2023-01-27 11:14:31 +0900

    allow picture

--- src/components/buttons/CreateButton.tsx
+++ src/components/buttons/CreateButton.tsx
@@ -24,7 +24,7 @@ const CreateButton = ({style, onModal}: TProps) => {
 
   const selectContent = async () => {
     const options: CameraOptions = {
-      mediaType: 'video',
+      mediaType: 'mixed',
       videoQuality: 'high',
       durationLimit: 30,
     };
