ca28f1f0197ddb079c06d4d877228baf86b83d83
Won Sang Choi <wonsang.choi@airballoon.app>
2023-02-08 21:07:49 +0900

    set up ios notification image

---
+++ ios/ImageNotification-Dev/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification-Dev
+//
+//  Created by Won Sang Choi on 2/8/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
