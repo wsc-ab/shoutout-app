ade88525cc4f87288505323e3338651338a11779
Won Sang Choi <wonsang.choi@airballoon.app>
2023-03-06 19:04:43 +0900

    set min video time to 5

--- src/utils/Video.ts
+++ src/utils/Video.ts
@@ -11,7 +11,7 @@ import {encodeToH264, generateThumb} from './Ffmpeg';
 
 import {createStoragePath, uploadFile} from './Storage';
 
-const duration = {max: 60, min: 10};
+const duration = {max: 60, min: 5};
 
 export const getVideo = async ({
   durationLimit,
